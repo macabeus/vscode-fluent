d487e6f58de1a7416970bd7567a5b78ee20bc8f4
macabeus <bruno.macabeus@gmail.com>
2021-10-26 22:47:22 +0100

    code-action: only show if there are fluent files

--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,4 @@
-import { TextDocument, Position } from 'vscode'
+import { TextDocument, Position, workspace } from 'vscode'
 
 const getIdentifierRangeAtPosition = (document: TextDocument, position: Position) =>
   document.getWordRangeAtPosition(position, /[a-zA-Z0-9-]+/)
@@ -6,4 +6,15 @@ const getIdentifierRangeAtPosition = (document: TextDocument, position: Position
 const fileNameEndsWithFtl = (document: TextDocument) =>
   document.fileName.endsWith('.ftl')
 
-export { getIdentifierRangeAtPosition, fileNameEndsWithFtl }
+const workspaceHasFtlFiles = async () => {
+  const ftlFiles = await workspace.findFiles('**/*.ftl')
+  const thereAreFtlFiles = ftlFiles.length > 0
+
+  return thereAreFtlFiles
+}
+
+export {
+  getIdentifierRangeAtPosition,
+  fileNameEndsWithFtl,
+  workspaceHasFtlFiles,
+}
