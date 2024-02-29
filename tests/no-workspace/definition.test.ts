2fb7bb602cdc9e3eb5eb20b9f2896e4516afc181
macabeus <bruno.macabeus@gmail.com>
2024-02-29 23:31:12 +0000

    chore: bump `eslint`

--- tests/no-workspace/definition.test.ts
+++ tests/no-workspace/definition.test.ts
@@ -2,13 +2,15 @@ const { Position, Range } = vscode
 
 describe('#Definition', () => {
   it('on message reference', () => {
-    return using({
-      files: {
-        'en.ftl': dedent(`
-          message = foo
-          message-interpolation = the { message } interpolation
-        `),
-      }},
+    return using(
+      {
+        files: {
+          'en.ftl': dedent(`
+            message = foo
+            message-interpolation = the { message } interpolation
+          `),
+        },
+      },
       async (mapFileToDoc) => {
         const definitions = await take.definitions(mapFileToDoc['en.ftl'], new Position(1, 32))
 
@@ -18,17 +20,20 @@ describe('#Definition', () => {
           targetRange: new Range(new Position(0, 0), new Position(0, 13)),
           targetSelectionRange: new Range(new Position(0, 0), new Position(0, 7)),
         })
-      })
+      }
+    )
   })
 
   it('on term reference', () => {
-    return using({
-      files: {
-        'en.ftl': dedent(`
-          -term = foo
-          term-interpolation = the { -term } interpolation
-        `),
-      }},
+    return using(
+      {
+        files: {
+          'en.ftl': dedent(`
+            -term = foo
+            term-interpolation = the { -term } interpolation
+          `),
+        },
+      },
       async (mapFileToDoc) => {
         const definitions = await take.definitions(mapFileToDoc['en.ftl'], new Position(1, 32))
 
@@ -38,21 +43,25 @@ describe('#Definition', () => {
           targetRange: new Range(new Position(0, 1), new Position(0, 11)),
           targetSelectionRange: new Range(new Position(0, 1), new Position(0, 5)),
         })
-      })
+      }
+    )
   })
 
   it('on variable', () => {
-    return using({
-      files: {
-        'en.ftl': dedent(`
-          variable-interpolation = the { $var } interpolation
-        `),
-      }},
+    return using(
+      {
+        files: {
+          'en.ftl': dedent(`
+            variable-interpolation = the { $var } interpolation
+          `),
+        },
+      },
       async (mapFileToDoc) => {
         const definitions = await take.definitions(mapFileToDoc['en.ftl'], new Position(0, 3))
 
         expect(definitions).toHaveLength(0)
-      })
+      }
+    )
   })
 })
 
